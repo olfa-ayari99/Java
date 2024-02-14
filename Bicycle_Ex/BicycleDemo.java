df8f3207591a221d742cd8892ddec5ce06805d8a
Olfa Ayari <olfa.ayari@esprit.tn>
2024-02-14 12:50:17 +0100

    Exemple classe

---
+++ Bicycle_Ex/BicycleDemo.java
@@ -0,0 +1,27 @@
+package Bicycle_Ex;
+
+
+  class BicycleDemo {
+    public static void main(String[] args) {
+
+        // Create two different 
+        // Bicycle objects
+        Bicycle bike1 = new Bicycle();
+        Bicycle bike2 = new Bicycle();
+
+        // Invoke methods on 
+        // those objects
+        bike1.changeCdence(50);
+        bike1.speedUp(10);
+        bike1.changeGear(2);
+        bike1.printStates();
+
+        bike2.changeCdence(50);
+        bike2.speedUp(10);
+        bike2.changeGear(2);
+        bike2.changeCdence(40);
+        bike2.speedUp(10);
+        bike2.changeGear(3);
+        bike2.printStates();
+    }
+}
